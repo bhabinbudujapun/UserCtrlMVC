f87d2ffc157ed606326d227511210468866971de
bhabinpun <poon.bhabin6332@gmail.com>
2023-01-04 17:12:38 +0545

    Directory|File->Structuring

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', '');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'http://localhost/UserControlMVC');
+
+// Site Name
+define('SITENAME', 'UserCtrl');
